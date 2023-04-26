432e0fd7d7f4f3fd71adb7ecdbd7830c74db2e95
Ubuntu <ubuntu@ip-172-31-35-11.ap-northeast-2.compute.internal>
2023-04-26 22:41:03 +0000

    priority scheduling 구현

--- threads/synch.c
+++ threads/synch.c
@@ -114,6 +114,7 @@ sema_up (struct semaphore *sema) {
 	old_level = intr_disable ();
 
 	if (!list_empty (&sema->waiters)){
+		list_sort(&sema->waiters, value_more_priority, NULL);
 		thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
 		sema->value++;
 		preempt_priority();
--- threads/thread.c
+++ threads/thread.c
@@ -533,6 +533,7 @@ next_thread_to_run (void) {
 	if (list_empty (&ready_list))
 		return idle_thread;
 	else
+		list_sort(&ready_list, value_more_priority, NULL);
 		return list_entry (list_pop_front (&ready_list), struct thread, elem);
 }
 
